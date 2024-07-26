88106fdb52d2330dc550c8ecc70039dd0a9d6e04
Ankit Kumar <ankitkmr0811@gmail.com>
2024-07-26 16:30:37 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>this project is a todo list application built with React.js. It allows users to add, delete, and update tasks, and it saves these tasks to the browser's local storage. The application is styled using HTML and CSS and javascript?</p>
+        </div>
+    )
+}
